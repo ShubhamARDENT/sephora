b57a26d66a251493a08f1f60584bb2db5606ce77
Shubham - ardent <shubham.chavan@ardentisys.com>
2024-12-10 18:50:44 +0530

    added js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,23 @@
 // Functionality of the page should appear here
 const productsApi =
-  "https://dummyjson.com/products?limit=12&select=title,description,price,rating,images ";
-const productName = document.querySelector(".bestseller_name")(async () => {
+  "https://dummyjson.com/products?limit=12&select=title,description,price,rating,images,thumbnail ";
+
+(async () => {
+  const productNames = document.querySelectorAll(".bestseller_name");
+  const productImges = document.querySelectorAll(".product_img");
+  const productDesc = document.querySelectorAll(".bestseller_desc");
+  const productPrice = document.querySelectorAll(".product_price");
   const productsData = await fetch(productsApi).then((response) =>
     response.json()
   );
 
   const { products } = productsData;
   console.log(products);
-  products.map((product) => {});
+  products.forEach((product, index) => {
+    productNames[index].innerHTML = product.title;
+    productImges[index].src = product.thumbnail;
+    const products = product.description.split(" ").slice(1, 10);
+    productDesc[index].innerHTML = products.join(" ").concat("...");
+    productPrice[index].innerHTML = `$${product.price}`;
+  });
 })();
