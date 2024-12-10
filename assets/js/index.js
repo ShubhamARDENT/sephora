1ea739977db5c9d21d49c430241d007dde140d12
Shubham - ardent <shubham.chavan@ardentisys.com>
2024-12-10 15:33:36 +0530

    js addeD

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,12 @@
 // Functionality of the page should appear here
+const productsApi =
+  "https://dummyjson.com/products?limit=12&select=title,description,price,rating,images ";
+const productName = document.querySelector(".bestseller_name")(async () => {
+  const productsData = await fetch(productsApi).then((response) =>
+    response.json()
+  );
+
+  const { products } = productsData;
+  console.log(products);
+  products.map((product) => {});
+})();
